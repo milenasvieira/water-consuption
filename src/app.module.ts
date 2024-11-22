4d44f2df7c3d2c072b4bf3dac6f2731fa68f2234
Milena Soares Vieira <milenasvieira63@gmail.com>
2024-11-21 23:22:03 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConsumoAguaModule } from './consumo-agua/consumo-agua.module';
 
 @Module({
-  imports: [ConsumoAguaModule, MongooseModule.forRoot('mongodb+srv://milena:071197@cluster-wb.eiarf.mongodb.net/webmobile?retryWrites=true&w=majority&appName=cluster-wb'), ConsumoAguaModule],
+  imports: [ConsumoAguaModule, MongooseModule.forRoot('mongodb+srv://<username>:<password>@cluster-wb.eiarf.mongodb.net/webmobile?retryWrites=true&w=majority&appName=cluster-wb'), ConsumoAguaModule],
   controllers: [AppController],
   providers: [AppService],
 })
